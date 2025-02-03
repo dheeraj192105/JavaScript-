781afdd3486d781b0287ab406fb4410a28aecfae
dheeraj192105 <dheerajkuwar16@gmail.com>
2025-02-03 21:08:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS Code as newer version.
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+//console.log(3 + 3)// code readability should be high
+
+console.log(typeof "dheeraj");
+
+let name = "aman" //this is string DataType
+let age = 20 // Numer datatype
+let isloggedIn = false //This is boolean DataType
+
+//  THIS ALL ARE PRIMITIVE DATATTYPES.
+// number => 2 to power 55
+// bigint
+// String is a basic datatype =>"" prefer to write in Double codes
+//boolean only assign two values => true/false.
+// null=> is a StandaLone value
+//undefined => means we are not assingned any value (Empty).
+// To finding uniqness we using the=> Symbol Value. implemnting mostly this in (React). 
+
+//object 
+
+console.log(typeof undefined); // this type is define a same undefined
+console.log(typeof null); // this type is a object.
