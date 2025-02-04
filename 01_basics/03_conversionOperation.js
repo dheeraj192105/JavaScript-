39280a15e3230aa175e365864a803c4a0af6f571
dheeraj192105 <dheerajkuwar16@gmail.com>
2025-02-04 21:03:55 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+ let valuenNumber = Number(score)
+// console.log(valuenNumber);
+
+
+// 1st we see converstion number "33" that is automatically covert in number 33.
+// next we see we give the value "33abc" this not convert in number this give Nan value(Not a number).
+// next we taked Boolean value True that was convert in 1 number; and false value covert in 0.
+
+
+let isLoggedIn = "dheeraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> we write 1 this give true value, 0=> we write 0 this give false value.
+// and we used empty "" strings the value we get =>False.
+// then we added the value in the strings the value will be => True.
+
+let someNumber = 2000
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
