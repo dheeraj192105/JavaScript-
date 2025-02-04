d96b4cf92433d012b9db44edb549de195563cb8d
dheeraj192105 <dheerajkuwar16@gmail.com>
2025-02-04 22:06:40 +0000

    Comparison of datatypes

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// log("2" > 1);console
+// console.log("02" > 1);
+
+// console.log(null > 0); => AVOID THIS TYPE OF CONVERSTION.
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0); =>THIS ALSO.
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// What is strict check - is checking the value with tripe === Equal.
+
+console.log("2" === 2);
+
+
+
